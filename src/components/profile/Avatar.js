2e392c560c72ecd33933ef521bcdf9618eb5638c
lionreddy <lionreddy220899@gmail.com>
2023-06-09 21:11:01 +0530

    raftlabs-commit

--- src/components/profile/Avatar.js
+++ src/components/profile/Avatar.js
@@ -4,14 +4,6 @@ import { Link } from "react-router-dom";
 
 export default function Avatar({ user, size = "xl", overrideAvatar = null }) {
   return (
-    // <ChakraAvatar
-    //   as={Link}
-    //   to={`${PROTECTED}/profile/${user.id}`}
-    //   name={user.username}
-    //   size={size}
-    //   src={overrideAvatar || user.avatar}
-    //   _hover={{ cursor: "pointer", opacity: "0.8" }}
-    // />
     <>
     <Link
   to={`${PROTECTED}/profile/${user.id}`}
